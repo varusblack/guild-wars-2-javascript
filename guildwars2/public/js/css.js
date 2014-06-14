a4b9b073153a7dabe25d8a53277c061b949f73f9
varusblack@gmail.com <varusblack@gmail.com@2da0f9ef-c745-2591-240a-e5d0f84f0f5f>
2014-06-14 17:33:48 +0000

    Css

---
+++ guildwars2/public/js/css.js
@@ -0,0 +1,64 @@
+//Funciones para cambiar el boton de Login
+function loginMouseOver() {
+	document.getElementById("button_login").className="login_mouseOver";
+}
+
+function loginMouseDown() {
+	document.getElementById("button_login").className="login_mouseDown";
+}
+
+function loginMouseOut() {
+	document.getElementById("button_login").className="login_mouseOut";
+}
+
+//Funciones para cambiar el boton de Registrarse
+function registerMouseOver() {
+	document.getElementById("button_register").className="register_mouseOver";
+}
+
+function registerMouseDown() {
+	document.getElementById("button_register").className="register_mouseDown";
+}
+
+function registerMouseOut() {
+	document.getElementById("button_register").className="register_mouseOut";
+}
+
+//Funciones para cambiar el boton de Cerrar sesión
+function logoutMouseOver() {
+	document.getElementById("button_logout").className="logout_mouseOver";
+}
+
+function logoutMouseDown() {
+	document.getElementById("button_logout").className="logout_mouseDown";
+}
+
+function logoutMouseOut() {
+	document.getElementById("button_logout").className="logout_mouseOut";
+}
+
+//Funciones para cambiar el boton de Ver perfil
+function profileMouseOver() {
+	document.getElementById("button_profile").className="profile_mouseOver";
+}
+
+function profileMouseDown() {
+	document.getElementById("button_profile").className="profile_mouseDown";
+}
+
+function profileMouseOut() {
+	document.getElementById("button_profile").className="profile_mouseOut";
+}
+
+//Funciones para cambiar el boton de guardar cambios
+function saveMouseOver() {
+	document.getElementById("button_save").className="save_mouseOver";
+}
+
+function saveMouseDown() {
+	document.getElementById("button_save").className="save_mouseDown";
+}
+
+function saveMouseOut() {
+	document.getElementById("button_save").className="save_mouseOut";
+}
